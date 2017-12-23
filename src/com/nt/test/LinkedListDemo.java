7026ab22348bd5229732b573bc63b87dd4cf238b
TL@gmail.com <Jaydev@192.168.0.6>
2017-12-23 20:16:15 +0530

    i LinkedList program

---
+++ src/com/nt/test/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.nt.test;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList l=new LinkedList();
+		l.add("ka");
+		l.add("kgjk");
+		System.out.println(l);
+
+	}
+
+}
