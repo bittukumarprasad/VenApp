efa89c3e40a33ad5d78532fc3479b1319f7a53e1
TL@gmail.com <Jaydev@192.168.0.6>
2017-12-23 10:55:23 +0530

    commited by jayadeba...no i am changing main method

--- src/com/app/controller/CustomerController.java
+++ src/com/app/controller/CustomerController.java
@@ -2,8 +2,7 @@ package com.app.controller;
 
 public class CustomerController {
 	public static void main(String[] args) {
-		
 		System.out.println("hi");
+		System.out.println("hi buttu my mirchi friend !!!how are you");
 	}
-
 }
