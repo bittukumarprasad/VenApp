e0e0936ddcd08f8aaa4a61f571786b882db26c1a
NareshIt <bittu.jsr.9852@gmail.com>
2017-12-24 22:47:43 +0530

    change your path ....and change username=scott password=tiger

--- src/com/nt/colletionPractice/HashMapDemo.java
+++ src/com/nt/colletionPractice/HashMapDemo.java
@@ -8,11 +8,15 @@ import java.util.Properties;
 public class HashMapDemo {
 
 	public static void main(String[] args) throws IOException {
-		Properties p=new Properties();
-		FileInputStream fis=new FileInputStream("abc.properties");
+		Properties p = new Properties();
+	//	FileInputStream fis = new FileInputStream(
+	//			"C:\\Users\\B.Prasad\\git\\VenApp\\src\\com\\app\\properties\\abc.properties");
+	
+		FileInputStream fis = new FileInputStream(
+					"C:\\Users\\B.Prasad\\git\\VenApp\\src\\com\\app\\properties\\abc.properties");
+		
 		p.load(fis);
 		System.out.println(p);
 	}
-	
-}
 
+}
